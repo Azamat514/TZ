32b3144197290dfce0a310422affca42715ef9ad
Azamat514 <zeero5873@gmail.com>
2025-11-15 19:04:54 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form");
+const input = document.querySelector(".js-input");
+const result = document.querySelector(".js-result");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text === "") {
+    result.textContent = "Matn kiriting!";
+    return;
+  }
+
+  const length = text.length;
+  const lower = text.toLowerCase();
+  const upper = text.toUpperCase();
+
+  result.innerHTML = `
+    Uzunligi: ${length} <br>
+    Kichik harflar: ${lower} <br>
+    Katta harflar: ${upper}
+  `;
+});
